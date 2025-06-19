8c119541a9e9c671316313578790236a8e19bbd9
sura721 <surafeladmas721@email.com>
2025-06-19 14:13:32 +0300

    correcting posts page

--- blog/app/posts/page.tsx
+++ blog/app/posts/page.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
-export const posts = () => {
+import Link from "next/link";
+export default function Home() {
   return (
-    <div>posts</div>
-  )
+    <main className="p-6 flex jestify-center items-center min-h-screen bg-gray-100">
+    <h1 className="text-3xl text-blue-800">This is the list of posts</h1>
+    </main>
+  );
 }
