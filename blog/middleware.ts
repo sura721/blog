addbcc4e176fc37dd2c1a814ce795cb94b5f5db5
sura721 <surafeladmas721@email.com>
2025-06-20 20:39:50 +0300

    updating middleware

--- blog/middleware.ts
+++ blog/middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)','/','/api/webhooks/clerk'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)','/','/api/webhooks/clerk','/api/uploadthing'])
 
 export default clerkMiddleware(async (auth, req) => {
-  // if (!isPublicRoute(req)) {
-  //   await auth.protect()
-  // }
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
 })
 
 export const config = {
