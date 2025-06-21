35ad95f2d9848540e25cdeedcfc2a9518595d4d8
sura721 <surafeladmas721@email.com>
2025-06-21 15:31:19 +0300

    correcting slug

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -1,22 +1,24 @@
+// next.config.js
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'utfs.io',
+        hostname: 'utfs.io', 
       },
       {
         protocol: 'https',
-        hostname: '*.ufs.sh',
+        hostname: '*.ufs.sh', 
       },
       {
         protocol: 'https',
-        hostname: 'img.clerk.com',
-      },
+        hostname: 'img.clerk.com', }
     ],
   },
 };
 
-export default nextConfig;
+export default withFlowbiteReact(nextConfig);
